66679d512d6fdf083b2a2d668a686f4c310046ef
Georgios Georgiou <ggeorgiou.gr@gmail.com>
2018-04-19 20:09:47 +0300

    Added the truncateString function

--- FCC-basic-algorithm-sriptint-js-files.js
+++ FCC-basic-algorithm-sriptint-js-files.js
@@ -126,3 +126,22 @@ function repeatStringNumTimes(str, num) {
    }
   return str;
 }
+
+
+
+/*
+* Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+* @param: a string, a number indicating the length of the string
+* @returns: The truncated string
+*/
+function truncateString(str, num) {
+  var mystr;
+  if (str.length > num && num > 3) {
+    mystr =  str.slice(0, (num-3))+"...";
+  } else if (str.length > num && num <=3) {
+    mystr = str.slice(0, num)+"...";
+  } else {
+    mystr =  str;
+  }
+  return mystr;
+}
